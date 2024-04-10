8bcbbf03773008df1924c075681c0b93f45690eb
swaroffski <dos-@mail.ru>
2024-04-10 17:35:21 +0300

    1.3

--- calculator.py
+++ calculator.py
@@ -1,15 +1,47 @@
 def calculate(expression):
-    a, operator, b = expression.split()
-    a, b = int(a), int(b)
-    if operator not in {'+', '-', '*', '/'} or not (1 <= a <= 10 and 1 <= b <= 10):
-        raise ValueError("Неверный формат математической операции или числа должны быть в диапазоне от 1 до 10 включительно")
-    return str(eval(expression))
+    try:
+        parts = expression.split()
+        if len(parts) != 3:
+            raise ValueError(
+                "throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)")
+
+        a, operator, b = parts
+        a = int(a)
+        b = int(b)
+
+        if operator not in ['+', '-', '*', '/']:
+            raise ValueError(
+                "throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)")
+
+        if not (1 <= a <= 10 and 1 <= b <= 10):
+            raise ValueError("throws Exception //т.к. числа должны быть в диапазоне от 1 до 10 включительно")
+
+        if operator == '+':
+            return str(a + b)
+        elif operator == '-':
+            return str(a - b)
+        elif operator == '*':
+            return str(a * b)
+        elif operator == '/':
+            return str(a // b)
+
+    except ValueError as e:
+        raise e
+
+
+def main(input_str):
+    return calculate(input_str)
+
 
 if __name__ == "__main__":
     try:
         while True:
-            user_input = input("Введите выражение (например, 2 + 3): ")
-            result = calculate(user_input)
-            print("Результат:", result)
-    except (KeyboardInterrupt, ValueError) as e:
+            user_input = input("Input:\n")
+            if len(user_input.split()) == 1:
+                raise ValueError("throws Exception //т.к. строка не является математической операцией")
+            result = main(user_input)
+            print("Output:\n", result)
+    except KeyboardInterrupt:
         print("\nПрограмма завершена.")
+    except ValueError as e:
+        print("Output:\n", e)
