627f9823d3e064230a56197dbbdd5bc28169e262
swaroffski <dos-@mail.ru>
2024-04-10 16:15:30 +0300

    1.3

--- calculator.py
+++ calculator.py
@@ -1,45 +1,15 @@
 def calculate(expression):
-    try:
-        parts = expression.split()
-        if len(parts) != 3:
-            raise ValueError("Неверный формат математической операции")
-
-        a, operator, b = parts
-        a = int(a)
-        b = int(b)
-
-        if operator not in ['+', '-', '*', '/']:
-            raise ValueError("Недопустимая операция")
-
-        if not (1 <= a <= 10 and 1 <= b <= 10):
-            raise ValueError("Числа должны быть в диапазоне от 1 до 10 включительно")
-
-        if operator == '+':
-            return str(a + b)
-        elif operator == '-':
-            return str(a - b)
-        elif operator == '*':
-            return str(a * b)
-        elif operator == '/':
-            return str(a // b)
-
-    except ValueError as e:
-        raise e
-
-
-def main(input_str):
-    return calculate(input_str)
-
+    a, operator, b = expression.split()
+    a, b = int(a), int(b)
+    if operator not in {'+', '-', '*', '/'} or not (1 <= a <= 10 and 1 <= b <= 10):
+        raise ValueError("Неверный формат математической операции или числа должны быть в диапазоне от 1 до 10 включительно")
+    return str(eval(expression))
 
 if __name__ == "__main__":
-    while True:
-        try:
+    try:
+        while True:
             user_input = input("Введите выражение (например, 2 + 3): ")
-            result = main(user_input)
+            result = calculate(user_input)
             print("Результат:", result)
-        except KeyboardInterrupt:
-            print("\nПрограмма завершена.")
-            break
-        except ValueError as e:
-            print("Ошибка:", e)
-            break
+    except (KeyboardInterrupt, ValueError) as e:
+        print("\nПрограмма завершена.")
