7701a8989789d3242cf73fff16e8303b2f1ea410
swaroffski <dos-@mail.ru>
2024-04-10 17:48:51 +0300

    1.4

--- calculator.py
+++ calculator.py
@@ -1,47 +1,43 @@
 def calculate(expression):
-    try:
-        parts = expression.split()
-        if len(parts) != 3:
-            raise ValueError(
-                "throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)")
+    parts = expression.split()
 
-        a, operator, b = parts
-        a = int(a)
-        b = int(b)
+    if len(parts) != 3:
+        raise ValueError(
+            "throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)")
 
-        if operator not in ['+', '-', '*', '/']:
-            raise ValueError(
-                "throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)")
+    a, operator, b = parts
+    a, b = int(a), int(b)
 
-        if not (1 <= a <= 10 and 1 <= b <= 10):
-            raise ValueError("throws Exception //т.к. числа должны быть в диапазоне от 1 до 10 включительно")
+    if operator not in {'+', '-', '*', '/'}:
+        raise ValueError(
+            "throws Exception //т.к. неверный оператор")
 
-        if operator == '+':
-            return str(a + b)
-        elif operator == '-':
-            return str(a - b)
-        elif operator == '*':
-            return str(a * b)
-        elif operator == '/':
-            return str(a // b)
+    if not (1 <= a <= 10 and 1 <= b <= 10):
+        raise ValueError("throws Exception //т.к. числа должны быть в диапазоне от 1 до 10 включительно")
 
-    except ValueError as e:
-        raise e
+    if operator == '+':
+        return str(a + b)
+    elif operator == '-':
+        return str(a - b)
+    elif operator == '*':
+        return str(a * b)
+    elif operator == '/':
+        return str(a // b)
 
 
-def main(input_str):
-    return calculate(input_str)
-
-
-if __name__ == "__main__":
+def main():
     try:
         while True:
             user_input = input("Input:\n")
             if len(user_input.split()) == 1:
                 raise ValueError("throws Exception //т.к. строка не является математической операцией")
-            result = main(user_input)
+            result = calculate(user_input)
             print("Output:\n", result)
     except KeyboardInterrupt:
         print("\nПрограмма завершена.")
     except ValueError as e:
         print("Output:\n", e)
+
+
+if __name__ == "__main__":
+    main()
